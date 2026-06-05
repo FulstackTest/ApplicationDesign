0530c2052ecab6d4277ebd8e6690ebfe1d1e63c7
pyj-0313 <pyjun0313@gmail.com>
2026-06-05 15:12:20 +0900

    [박영준] V0.0 BasicDataSource(commons-dbcp2) 빈 등록

--- EX01/src/main/java/com/example/demo/Config/DataSourceConfig.java
+++ EX01/src/main/java/com/example/demo/Config/DataSourceConfig.java
@@ -1,6 +1,11 @@
 package com.example.demo.Config;
 
+import com.zaxxer.hikari.HikariDataSource;
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 // TODO: DataSource 빈 2개를 작성하라.
 //  필요한 import : javax.sql.DataSource, org.springframework.context.annotation.Bean,
@@ -11,6 +16,16 @@ public class DataSourceConfig {
     // TODO 1) @Bean dataSource2() : BasicDataSource(commons-dbcp2)
     //        - setDriverClassName("com.mysql.cj.jdbc.Driver")
     //        - setUrl("jdbc:mysql://localhost:3306/testdb"), setUsername("root"), setPassword("1234")
+    @Bean
+    public DataSource dataSource2(){
+        BasicDataSource dataSource = new BasicDataSource(); // commons-dbcp2 커넥션 풀 생성
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver"); // My SQL 드라이버 설정
+        dataSource.setUrl("jdbc:mysql://localhost:3306/testdb"); // DB접속 URL 설정
+        dataSource.setUsername("root"); // DB 사용자명
+        dataSource.setPassword("1234"); // DB 비밀번호
+        return dataSource; // dataSource 반환
+
+    }
     // TODO 2) @Bean dataSource3() : HikariDataSource
     //        - setDriverClassName("com.mysql.cj.jdbc.Driver")
     //        - setJdbcUrl("jdbc:mysql://localhost:3306/testdb"), setUsername("root"), setPassword("1234")
