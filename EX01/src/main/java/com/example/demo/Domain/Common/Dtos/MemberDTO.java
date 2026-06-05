e0d6125ae4afe78c8fae1c878b2ebac08a59daf6
DESKTOP-FU2AS1G\Administrator <byeongseon8@gamil.com>
2026-06-05 14:37:42 +0900

    V0.2 애너테이션 추가

--- EX01/src/main/java/com/example/demo/Domain/Common/Dtos/MemberDTO.java
+++ EX01/src/main/java/com/example/demo/Domain/Common/Dtos/MemberDTO.java
@@ -18,16 +18,16 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Builder
 public class MemberDTO {
-//
-//    // TODO: 각 필드에 알맞은 검증 애너테이션을 추가하라.
-//    private Long id;                  // 식별자 (검증 없음, DB AUTO_INCREMENT)
-//    @NotBlank(message = "이름은 필수입니다.")
-//    private String name;              // TODO: @NotBlank
-//    @NotBlank(message = "이메일은 필수입니다.")
-//    @Email(message = "올바른 이메일 형식이어야 합니다.")
-//    private String email;             // TODO: @NotBlank + @Email
-//    @NotBlank(message = "전화번호 입력은 필수입니다.")
-//    private String phone;             // TODO: @NotBlank
-//    private LocalDateTime createAt;   // 등록일시 (검증 없음)
+
+    // TODO: 각 필드에 알맞은 검증 애너테이션을 추가하라.
+    private Long id;                  // 식별자 (검증 없음, DB AUTO_INCREMENT)
+    @NotBlank(message = "이름은 필수입니다.")
+    private String name;              // TODO: @NotBlank
+    @NotBlank(message = "이메일은 필수입니다.")
+    @Email(message = "올바른 이메일 형식이어야 합니다.")
+    private String email;             // TODO: @NotBlank + @Email
+    @NotBlank(message = "전화번호 입력은 필수입니다.")
+    private String phone;             // TODO: @NotBlank
+    private LocalDateTime createAt;   // 등록일시 (검증 없음)
 
 }
