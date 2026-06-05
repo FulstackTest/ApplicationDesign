8c8a53f0d117194313e7e5b80cd9897ca3c22dd9
DESKTOP-FU2AS1G\Administrator <byeongseon8@gamil.com>
2026-06-05 14:36:21 +0900

    V0.1 getter/setter/builder 생성

--- EX01/src/main/java/com/example/demo/Domain/Common/Dtos/MemberDTO.java
+++ EX01/src/main/java/com/example/demo/Domain/Common/Dtos/MemberDTO.java
@@ -12,12 +12,12 @@ import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
-//// TODO: 아래 클래스에 Lombok 애너테이션을 붙여 getter/setter/builder 가 생성되도록 하라.
-//@Data
-//@NoArgsConstructor
-//@AllArgsConstructor
-//@Builder
-//public class MemberDTO {
+// TODO: 아래 클래스에 Lombok 애너테이션을 붙여 getter/setter/builder 가 생성되도록 하라.
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class MemberDTO {
 //
 //    // TODO: 각 필드에 알맞은 검증 애너테이션을 추가하라.
 //    private Long id;                  // 식별자 (검증 없음, DB AUTO_INCREMENT)
@@ -30,4 +30,4 @@ import java.time.LocalDateTime;
 //    private String phone;             // TODO: @NotBlank
 //    private LocalDateTime createAt;   // 등록일시 (검증 없음)
 
-//}
+}
