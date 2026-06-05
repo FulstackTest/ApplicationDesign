a5bd14c2a93677edc3d0ef282b7946b7353a277a
1gyeolpark <marine3682@gmail.com>
2026-06-05 14:46:02 +0900

    [박한결] V0.0 미존재파일 예외 처리

--- EX01/src/main/java/com/example/demo/Controller/GlobalException/GlobalExceptionHandler.java
+++ EX01/src/main/java/com/example/demo/Controller/GlobalException/GlobalExceptionHandler.java
@@ -1,14 +1,29 @@
 package com.example.demo.Controller.GlobalException;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.io.FileNotFoundException;
 
 // TODO: 전역 예외 핸들러 3개를 작성하라.
 //  필요한 import : org.springframework.ui.Model,
 //                 org.springframework.web.bind.annotation.ExceptionHandler, java.io.FileNotFoundException
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     // TODO 1) @ExceptionHandler(FileNotFoundException.class) -> model 에 담고 "global/error1" 반환
+    // FileNotFoundException: 미존재 파일 예외 처리
+    // 시스템에서 파일을 찾을 수 없을 때 발생하는 예외를 처리한다.
+    @ExceptionHandler(FileNotFoundException.class)
+    public String exceptionHandler_1(Exception e, Model model){
+        log.error("[Global] ExceptionTestController ex1 : " + e);   // 에러로그 출력
+        model.addAttribute("e",e);  // 에러로그를 객체로 view에 전달
+        return "global/error1"; //  에러 페이지로 이동
+    }
+
     // TODO 2) @ExceptionHandler(ArithmeticException.class)   -> "global/error2" 반환
     // TODO 3) @ExceptionHandler(Exception.class)             -> "global/error3" 반환
 
